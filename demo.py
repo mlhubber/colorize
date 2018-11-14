5c1620b3168a5fe42cc948b2b7137f7cac45f149
Graham Williams <graham.williams@togaware.com>
2018-11-14 15:38:23 +0800

    Minor user experience update.

--- demo.py
+++ demo.py
@@ -12,6 +12,7 @@ cwd = os.getcwd()
 print("Demonstrate colorization using images found in\n",
       os.path.join(cwd, IMG_PATH), "\n")
 
+print("Please close each image (Ctrl-w) to proceed through the demonstration.\n")
 
 if __name__ == '__main__':
     image_folder = 'images'
